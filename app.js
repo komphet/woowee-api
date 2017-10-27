2345ea4f7d016af28e5a26e829ac2e8b911ed8dc
komphet <komphet@kmi.tl>
2017-10-27 11:34:07 +0700

    Update API

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const fs = require("fs");
 const WavDecoder = require("wav-decoder");
 var assert = require('assert');
 const config = require('./config');
+import {Note, Pitcher} from 'pitch-detector';
 
 var connection = mysql.createConnection({
     host: config.DB_HOST,
@@ -16,6 +17,8 @@ var connection = mysql.createConnection({
 });
 var upload = multer({dest: 'public/uploads/'})
 
+var MIN_SAMPLES = 0;  // will be initialized when AudioContext is created.
+var GOOD_ENOUGH_CORRELATION = 0.9; // this is the "bar" for how close a correlation needs to be
 
 connection.connect()
 app.use(bodyParser.urlencoded({
@@ -31,7 +34,7 @@ app.post('/api/login', function (req, res) {
         '"', function (err, rows, fields) {
             if (err) console.error(err)
             if (rows[0] == null) {
-                connection.query('INSERT INTO users VALUES(NULL,"' + req.body.fbId + '","' + req.body.email + '","' + req.body.username + '","",NULL);', function (err2, rows2, fields2) {
+                connection.query('INSERT INTO users VALUES(NULL,"' + req.body.fbId + '","' + req.body.email + '","' + req.body.username + '","",NULL,NULL);', function (err2, rows2, fields2) {
                         if (err2) console.error(err2)
                         res.send(rows2)
                     }
@@ -43,18 +46,71 @@ app.post('/api/login', function (req, res) {
     )
 })
 
+function autoCorrelate(buf, sampleRate) {
+    var SIZE = buf.length;
+    var MAX_SAMPLES = Math.floor(SIZE / 2);
+    var best_offset = -1;
+    var best_correlation = 0;
+    var rms = 0;
+    var foundGoodCorrelation = false;
+    var correlations = new Array(MAX_SAMPLES);
+
+    for (var i = 0; i < SIZE; i++) {
+        var val = buf[i];
+        rms += val * val;
+    }
+    rms = Math.sqrt(rms / SIZE);
+    if (rms < 0.01) // not enough signal
+        return -1;
+
+    var lastCorrelation = 1;
+    for (var offset = MIN_SAMPLES; offset < MAX_SAMPLES; offset++) {
+        var correlation = 0;
+
+        for (var i = 0; i < MAX_SAMPLES; i++) {
+            correlation += Math.abs((buf[i]) - (buf[i + offset]));
+        }
+        correlation = 1 - (correlation / MAX_SAMPLES);
+        correlations[offset] = correlation; // store it, for the tweaking we need to do below.
+        if ((correlation > GOOD_ENOUGH_CORRELATION) && (correlation > lastCorrelation)) {
+            foundGoodCorrelation = true;
+            if (correlation > best_correlation) {
+                best_correlation = correlation;
+                best_offset = offset;
+            }
+        } else if (foundGoodCorrelation) {
+            // short-circuit - we found a good correlation, then a bad one, so we'd just be seeing copies from here.
+            // Now we need to tweak the offset - by interpolating between the values to the left and right of the
+            // best offset, and shifting it a bit.  This is complex, and HACKY in this code (happy to take PRs!) -
+            // we need to do a curve fit on correlations[] around best_offset in order to better determine precise
+            // (anti-aliased) offset.
+
+            // we know best_offset >=1,
+            // since foundGoodCorrelation cannot go to true until the second pass (offset=1), and
+            // we can't drop into this clause until the following pass (else if).
+            var shift = (correlations[best_offset + 1] - correlations[best_offset - 1]) / correlations[best_offset];
+            return sampleRate / (best_offset + (8 * shift));
+        }
+        lastCorrelation = correlation;
+    }
+    if (best_correlation > 0.01) {
+        // console.log("f = " + sampleRate/best_offset + "Hz (rms: " + rms + " confidence: " + best_correlation + ")")
+        return sampleRate / best_offset;
+    }
+    return -1;
+//	var best_frequency = sampleRate/best_offset;
+}
 
 var saveFile = function (req, res, action) {
     if (req.file) {
         var fileDetail = req.file;
-        if (action == "init") {
-            const buffer = fs.readFileSync("./" + fileDetail.destination + "/" + fileDetail.filename);
-            const decoded = WavDecoder.decode(buffer);
-            var toneRate, tone = "-", fq;
-            decoded.then(function (bufferDecoded) {
-                const float32Array = bufferDecoded.channelData[0]; // get a single channel of sound
-                fq = (bufferDecoded.sampleRate / (float32Array.sort()[float32Array.length - 1] - float32Array.sort()[0])) / 100
-                console.log(fq);
+
+        const buffer = fs.readFileSync("./" + fileDetail.destination + "/" + fileDetail.filename);
+        const decoded = WavDecoder.decode(buffer);
+        var toneRate, tone = "-", fq;
+        decoded.then(function (bufferDecoded) {
+            fq = Math.floor(autoCorrelate(bufferDecoded.channelData[1], bufferDecoded.sampleRate));
+            if (action == "init") {
                 toneRate = {
                     f: {
                         h: [617, null],
@@ -89,40 +145,10 @@ var saveFile = function (req, res, action) {
                     }
 
                 });
+            }
 
-
-                connection.query('INSERT INTO files VALUES(NULL,"' +
-                    req.body.fbId + '","'
-                    + fileDetail.fieldname
-                    + '","'
-                    + fileDetail.originalname
-                    + '","'
-                    + fileDetail.encoding
-                    + '","'
-                    + fileDetail.mimetype
-                    + '","'
-                    + fileDetail.destination
-                    + '","'
-                    + fileDetail.filename
-                    + '","'
-                    + fileDetail.path
-                    + '","'
-                    + fileDetail.size
-                    + '","' + tone + '");', function (err2, results, fields2) {
-                        if (err2) console.error(err2)
-                        fileDetail.tone = tone;
-                        fileDetail.id = results.insertId;
-                        connection.query('UPDATE users SET gender = "' + req.body.gender + '", init_voice = "' + fileDetail.id + '" WHERE fb_id = "' + req.body.fbId + '";', function (err2, results, fields2) {
-                                if (err2) console.error(err2)
-                            }
-                        )
-                        return res.send(fileDetail)
-                    }
-                )
-            })
-        } else if (action == "chat") {
             connection.query('INSERT INTO files VALUES(NULL,"' +
-                req.body.sender + '","'
+                req.body.fbId + '","'
                 + fileDetail.fieldname
                 + '","'
                 + fileDetail.originalname
@@ -138,24 +164,32 @@ var saveFile = function (req, res, action) {
                 + fileDetail.path
                 + '","'
                 + fileDetail.size
-                + '","");', function (err2, results, fields2) {
+                + '","' + fq + '");', function (err2, results, fields2) {
                     if (err2) console.error(err2)
+                    fileDetail.tone = tone;
+                    fileDetail.fq = fq;
                     fileDetail.id = results.insertId;
-                    connection.query('UPDATE chats SET is_new = 0 WHERE sender = "' + req.body.sender + '" AND receiver = "' + req.body.receiver + '";', function (err2, results, fields2) {
-                            if (err2) console.error(err2)
-                            connection.query('INSERT INTO chats VALUES(NULL,' + req.body.sender + ',' + req.body.receiver + ',1,' + fileDetail.id + ',NULL);', function (err2, results, fields2) {
-                                    if (err2) console.error(err2)
-                                }
-                            )
-                        }
-                    )
-
+                    if (action == "init") {
+                        connection.query('UPDATE users SET gender = "' + req.body.gender + '", init_voice = "' + fileDetail.id + '", tone = "' + tone + '" WHERE fb_id = "' + req.body.fbId + '";', function (err2, results, fields2) {
+                                if (err2) console.error(err2)
+                            }
+                        )
+                    } else if (action == "chat") {
+                        connection.query('UPDATE chats SET is_new = 0 WHERE sender = "' + req.body.sender + '" AND receiver = "' + req.body.receiver + '";', function (err2, results, fields2) {
+                                if (err2) console.error(err2)
+                                connection.query('INSERT INTO chats VALUES(NULL,' + req.body.sender + ',' + req.body.receiver + ',1,' + fileDetail.id + ',NULL);', function (err2, results, fields2) {
+                                        if (err2) console.error(err2)
+                                    }
+                                )
+                            }
+                        )
+                    }
                     return res.send(fileDetail)
                 }
             )
-        }
-
-    } else {
+        })
+    }
+    else {
         res.send('Missing file');
     }
 }
@@ -165,7 +199,7 @@ app.post('/api/voiceinit', upload.single('file'), function (req, res) {
 })
 
 app.post('/api/find', function (req, res) {
-    connection.query('SELECT * FROM users WHERE init_voice IS NOT NULL ', function (err, rows, fields) {
+    connection.query('SELECT * FROM users WHERE init_voice IS NOT NULL AND gender = "' + req.body.gender + '" AND tone = "' + req.body.tone + '" ', function (err, rows, fields) {
             if (err) console.error(err)
             res.send(rows[Math.floor(Math.random() * rows.length)])
         }
@@ -192,20 +226,43 @@ app.post('/api/getvoice', function (req, res) {
             }
         )
     } else {
-        connection.query('SELECT * FROM chats WHERE sender = ' + req.body.sender + ' AND receiver = ' + req.body.receiver + ' ORDER BY id DESC LIMIT 0,1;', function (err, rows, fields) {
+        var backstep = (typeof req.body.backstep != "undefined") ? req.body.backstep : 0;
+        console.log(backstep);
+        connection.query('SELECT * FROM chats WHERE sender = ' + req.body.sender + ' AND receiver = ' + req.body.receiver + ' ORDER BY id DESC LIMIT ' + backstep + ',1;', function (err, rows, fields) {
                 if (err) console.error(err)
-                connection.query('UPDATE chats SET is_new = 0 WHERE sender = "' + req.body.sender + '" AND receiver = "' + req.body.receiver + '";', function (err2, results, fields2) {
-                        if (err2) console.error(err2)
+                if (rows.length == 0) {
+                    res.send({"error": "No File"})
+                } else {
+                    if (backstep == 0) {
+                        connection.query('UPDATE chats SET is_new = 0 WHERE sender = "' + req.body.sender + '" AND receiver = "' + req.body.receiver + '";', function (err2, results, fields2) {
+                                if (err2) console.error(err2)
+                                connection.query('SELECT * FROM files WHERE id = ' + rows[0].file + ';', function (err2, rows2, fields2) {
+                                        if (err2) console.error(err2)
+                                        if (rows2.length != 0) {
+                                            var filepath = rows2[0].destination + rows2[0].filename;
+                                            res.set({'Content-Type': 'audio/wav'});
+                                            var readStream = fs.createReadStream(filepath);
+                                            readStream.pipe(res);
+                                        } else {
+                                            res.send(JSON.parse({'error': 'No File'}))
+                                        }
+                                    }
+                                )
+                            }
+                        )
+                    } else {
                         connection.query('SELECT * FROM files WHERE id = ' + rows[0].file + ';', function (err2, rows2, fields2) {
                                 if (err2) console.error(err2)
-                                filepath = rows2[0].destination + "/" + rows2[0].filename;
+
+                                var filepath = rows2[0].destination + rows2[0].filename;
                                 res.set({'Content-Type': 'audio/wav'});
                                 var readStream = fs.createReadStream(filepath);
                                 readStream.pipe(res);
+
                             }
                         )
                     }
-                )
+                }
             }
         )
     }
@@ -228,10 +285,10 @@ app.post('/api/liked', function (req, res) {
             if (err) console.error(err)
             var fbId = [];
             rows.forEach(function (value, index) {
-                if(fbId.indexOf(value.receiver) == -1 && value.receiver != req.body.fbId){
+                if (fbId.indexOf(value.receiver) == -1 && value.receiver != req.body.fbId) {
                     fbId.push(value.receiver)
                 }
-                if(fbId.indexOf(value.sender) == -1 && value.sender != req.body.fbId){
+                if (fbId.indexOf(value.sender) == -1 && value.sender != req.body.fbId) {
                     fbId.push(value.sender)
                 }
             })
