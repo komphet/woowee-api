ef5df971c1194e872960aa8990d2f260ab9ceac0
komphet <komphet@kmi.tl>
2017-10-27 22:05:53 +0700

    Update API

--- app.js
+++ app.js
@@ -61,6 +61,10 @@ app.post('/api/login', function (req, res) {
 
 function autoCorrelate(buf, sampleRate) {
     var SIZE = buf.length;
+    if (SIZE > 100000) {
+        SIZE = 100000;
+    }
+    console.log("size " + SIZE);
     var MAX_SAMPLES = Math.floor(SIZE / 2);
     var best_offset = -1;
     var best_correlation = 0;
@@ -71,17 +75,22 @@ function autoCorrelate(buf, sampleRate) {
     for (var i = 0; i < SIZE; i++) {
         var val = buf[i];
         rms += val * val;
+        console.log(i, rms);
     }
     rms = Math.sqrt(rms / SIZE);
+    console.log(rms);
     if (rms < 0.01) // not enough signal
         return -1;
 
     var lastCorrelation = 1;
     for (var offset = MIN_SAMPLES; offset < MAX_SAMPLES; offset++) {
         var correlation = 0;
+        console.log("Offset ", offset);
 
         for (var i = 0; i < MAX_SAMPLES; i++) {
             correlation += Math.abs((buf[i]) - (buf[i + offset]));
+            console.log("MAX_SAMPLES  ", i, correlation);
+
         }
         correlation = 1 - (correlation / MAX_SAMPLES);
         correlations[offset] = correlation; // store it, for the tweaking we need to do below.
@@ -125,7 +134,7 @@ var saveFile = function (req, res, action) {
             console.log("Calculating FQ");
             // console.log(bufferDecoded.channelData[0]);
             fq = Math.floor(autoCorrelate(bufferDecoded.channelData[0], bufferDecoded.sampleRate));
-            console.log("FQ="+fq);
+            console.log("FQ=" + fq);
             connection.query('SELECT * FROM setup WHERE slug = "TONE_RATE";', function (err, rows, fields) {
                 if (rows.length != 0) {
                     toneRate = JSON.parse(rows[0].value);
